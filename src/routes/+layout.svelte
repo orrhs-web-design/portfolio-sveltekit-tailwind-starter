ee5b6694bf3d510644d5aa11de0c7a098cb149b5
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-03-15 11:47:53 -0400

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
-<!-- Tailwind CSS Required page. It is a wrapper around all of the other content and applies styles found in app.css. Don't touch unless you are sure of what you are doing. -->
+<!--This is a wrapper around all other content on your website. Things here
+should be on every page, such as a header, footer, or the loading of global
+styles.-->
+<!--Also required by Tailwind CSS to load in the global Tailwind Styles.-->
 <script>
   import "../app.css";
+	import Footer from '$lib/components/Footer.svelte';
+	import Header from '$lib/components/Header.svelte';
 </script>
 
+<Header />
+
+<!--This is the main content on every page-->
 <slot />
+
+<Footer />
